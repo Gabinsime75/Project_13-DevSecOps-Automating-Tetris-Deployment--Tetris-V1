0e2d49edd32600ef03e406ecd5283bdbc98930fb
gabin-sime <gabin75@yahoo.de>
2024-03-14 00:14:18 -0500

    updated the eks-terraform file

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -22,15 +22,15 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
 }
 
 #get vpc data
-data "aws_vpc" "default" {
-  default = true
+data "aws_vpc" "Default-2-vpc" {
+  tags = {
+    Name = "Default-2-vpc"
+  }
 }
 #get public subnets for cluster
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+data "aws_subnet_ids" "public_subnets" {
+  vpc_id = data.aws_vpc.Default-2-vpc.id
+
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
@@ -38,7 +38,7 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.public.ids
+    subnet_ids = data.aws_subnets.public_subnets.ids
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
@@ -83,7 +83,7 @@ resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "Node-cloud"
   node_role_arn   = aws_iam_role.example1.arn
-  subnet_ids      = data.aws_subnets.public.ids
+  subnet_ids      = data.aws_subnets.public_subnets.ids
 
   scaling_config {
     desired_size = 1
