57e03ca5d09bd4721ceec2fc390f7ddf3077a2e6
Gabin Sime <115108532+Gabinsime75@users.noreply.github.com>
2024-03-16 01:49:32 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tetrisprojbucket" # Replace with your actual S3 bucket name
+    bucket = "tetrisprojbucket-v1" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
